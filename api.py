0f5d412842c639dfc821ca8471d647d5538d73f1
Amarpreet Singh <amarpreet2209@gmail.com>
2018-08-09 02:53:01 +0530

    procfile updated 2

--- api.py
+++ api.py
@@ -33,9 +33,9 @@ def example():
 		text = transcript.text
 	return text
 
-if __name__ == '__main__':
+
   
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get('PORT', 5000))
+app.run(port=port)
 
 
