7783bc71687e9b49a3c9237c0575282c7238f897
Amarpreet Singh <amarpreet2209@gmail.com>
2018-08-09 00:50:43 +0530

    first commit

---
+++ api.py
@@ -0,0 +1,26 @@
+from flask import Flask,request
+app = Flask(__name__)
+
+# import speech_recognition as sr
+# r = sr.Recognizer()
+
+import assemblyai
+aai = assemblyai.Client(token='39f8f26db4c546c99308fba032eda3dd')
+
+
+
+@app.route('/', methods=["POST"])
+def example():
+	# harvard = sr.AudioFile('house.wav')
+	# with harvard as source:
+ #  		audio = r.record(source)
+	# text = r.recognize_google(audio)
+	f = request.files['file']
+	transcript = aai.transcribe(filename=f.filename)
+	while transcript.status != 'completed':
+		transcript = transcript.get()
+	text = transcript.text
+
+	return text
+app.run()
+
